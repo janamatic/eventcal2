0cc330354eaa20cfa0f99076875ca592bd04f914
sb <sb@ubuntu.(none)>
2013-05-19 11:25:57 -0400

    add search, change layout

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :tag_list, :address1, :address2, :start_time, :city, :description, :event_date, :latitude, :longitude, :state, :title, :zip, :gmaps
+  attr_accessible :photo, :tag_list, :address1, :address2, :start_time, :city, :description, :event_date, :latitude, :longitude, :state, :title, :zip, :gmaps
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "/images/:style/missing.png"
   acts_as_gmappable
 
